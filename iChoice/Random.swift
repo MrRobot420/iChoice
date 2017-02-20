bbb0d387f7f8dac989eb9a617a7ed97c54a69c67
MrRobot420 <max.karlchen@web.de>
2017-02-21 00:01:27 +0100

    Random Klasse

---
+++ iChoice/Random.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  iChoice
+//
+//  Created by Maximilian Karl on 20.02.17.
+//  Copyright © 2017 Robot. All rights reserved.
+//
+
+import Foundation
+
+class Random{
+    func checkDayTime(dayTime : String)-> String{
+        switch dayTime{
+        case morgens:
+        case mittags:
+        case abends:
+        }
+    }
+}
